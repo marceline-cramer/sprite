755a2757b98e1f6411b846e1e96534861463b80f
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-29 22:29:48 -0600

    Add Module

---
+++ src/core/Module.cc
@@ -0,0 +1,32 @@
+/**
+ * @file Module.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Implements a Structure.
+ * @date 2020-10-29
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This file is part of Sprite.
+ *
+ * Sprite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sprite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Sprite.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#include "core/Module.h"
+
+namespace sprite {
+
+Module::Module() {}
+
+}  // namespace sprite
